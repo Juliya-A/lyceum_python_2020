fe37b0b69831bebd6a74e76b6636783df4e81ac6
Juliya-A <61663429+Juliya-A@users.noreply.github.com>
2020-03-09 20:04:34 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,8 @@
-int x = 1 #число в ряду Фиббоначи с позицией 1
-int y = 2 #число в ряду Фиббоначи с позицией 2
-n = input("Введите позицию числа из ряда Фиббоначи")
+a = int(input("Введите порядковый номер числа Фиббоначи: "))
 
-fib (n, x, y)
- if n==0
- return 0
- elif n==1
- return 1
- elif n==2
- return 1
-
-else:
-return fib(n-1) + fib(n-2)
+def fib(a):
+    if a==1 or a==2:
+        return 1
+    else:
+        return fib(a-2) + fib(a-1)
+print(fib(a))
